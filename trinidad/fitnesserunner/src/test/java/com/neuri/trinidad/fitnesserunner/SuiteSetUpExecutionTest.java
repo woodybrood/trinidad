e7bcfbb7befd6f7803dbdb6ad44a9154e310f7c7
gojkoa <gojkoa@d5e6a7ba-fa0c-11dd-99c0-75d572ba1ddd>
2009-03-04 08:34:55 +0000

    1.0.5 - suitesetup and teardown executed for suites and single tests

---
+++ trinidad/fitnesserunner/src/test/java/com/neuri/trinidad/fitnesserunner/SuiteSetUpExecutionTest.java
@@ -0,0 +1,25 @@
+package com.neuri.trinidad.fitnesserunner;
+
+import java.util.List;
+
+import junit.framework.Assert;
+
+import com.neuri.trinidad.Test;
+
+
+public class SuiteSetUpExecutionTest {
+	
+	@org.junit.Test
+	public void suiteSetUpIsExecutedOnSuite() throws Exception{
+		FitNesseRepository frp=new FitNesseRepository("target/test-classes");
+		List<Test> tests=frp.getSuite("SlimTest");
+		Assert.assertEquals("SlimTest.SuiteSetUp", tests.get(0).getName());
+	}
+	@org.junit.Test
+	public void suiteSetUpIsExecutedOnASingleTest() throws Exception{
+		FitNesseRepository frp=new FitNesseRepository("target/test-classes");
+		Test t=frp.getTest("SlimTest.PlayerList");
+		
+		Assert.assertTrue(t.getContent().contains("this is in suite set up"));		
+	}
+}
