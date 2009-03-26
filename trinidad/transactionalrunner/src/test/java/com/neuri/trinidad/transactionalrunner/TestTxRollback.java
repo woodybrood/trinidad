d8524263b73e5cdece94a230e82c9f953b349dba
bbeskow <bbeskow@d5e6a7ba-fa0c-11dd-99c0-75d572ba1ddd>
2009-03-26 14:29:14 +0000

    Use an in-memory Derby database and create the 'TEST' table in the setup method, to make the test fully autonomous.

--- trinidad/transactionalrunner/src/test/java/com/neuri/trinidad/transactionalrunner/TestTxRollback.java
+++ trinidad/transactionalrunner/src/test/java/com/neuri/trinidad/transactionalrunner/TestTxRollback.java
@@ -3,15 +3,40 @@ package com.neuri.trinidad.transactionalrunner;
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.junit.Before;
 
 import com.neuri.trinidad.transactionalrunner.FitnesseSpringContext;
 import com.neuri.trinidad.transactionalrunner.RollbackIntf;
 import com.neuri.trinidad.transactionalrunner.RollbackNow;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class TestTxRollback {
+
+	@Before
+	public void createTestTable() throws Exception {
+		System.setProperty("spring.context", "classpath:spring.xml");
+		DataSource dataSource = (DataSource) FitnesseSpringContext.getInstance().getBean("dataSource");
+		Connection conn = dataSource.getConnection();
+		PreparedStatement ps = null;
+		try {
+			try {
+			ps = conn.prepareStatement("DROP TABLE test");
+			ps.executeUpdate();
+			ps.close();
+			} catch (Exception e) {}
+			ps = conn.prepareStatement("CREATE TABLE test (name varchar(100))");
+			ps.executeUpdate();
+			ps.close();
+		} finally {
+			conn.close();
+		}
+	}
+
 	@Test
 	public void testRollsBack(){
-		System.setProperty("spring.context", "classpath:spring.xml");
 		RollbackIntf bn= FitnesseSpringContext.getRollbackBean();
 		final TestRepository tr=(TestRepository) FitnesseSpringContext.getInstance().getBean("testRepository");
 		try {
