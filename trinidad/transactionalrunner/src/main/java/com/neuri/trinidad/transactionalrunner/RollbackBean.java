a28ae0ca956a049f3faac8450efe0d966b7d01a9
andygodwin <andygodwin@gmail.com>
2009-03-31 14:38:16 +0000

    Extracted system property name to constant

--- trinidad/transactionalrunner/src/main/java/com/neuri/trinidad/transactionalrunner/RollbackBean.java
+++ trinidad/transactionalrunner/src/main/java/com/neuri/trinidad/transactionalrunner/RollbackBean.java
@@ -5,11 +5,13 @@ import java.util.concurrent.Executor;
 import org.springframework.transaction.annotation.Transactional;
 
 public class RollbackBean implements RollbackIntf {
+    private static final String TEST_WRAPPER_BEAN_NAME = "trinidad.test.wrapper";
+
     @Transactional(rollbackFor = Throwable.class)
     public void process(Runnable r) {
-        if (System.getProperty("trinidad.test.wrapper") != null) {
+        if (System.getProperty(TEST_WRAPPER_BEAN_NAME) != null) {
             Executor testWrapper = (Executor) FitnesseSpringContext.getInstance().getBean(
-                    System.getProperty("trinidad.test,wrapper"));
+                    System.getProperty(TEST_WRAPPER_BEAN_NAME));
             testWrapper.execute(r);
         } else {
             r.run();
