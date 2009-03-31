6ed7762a8a9c9038f0b0e6a2b332aaee005be6ad
andygodwin@gmail.com <andygodwin@gmail.com@d5e6a7ba-fa0c-11dd-99c0-75d572ba1ddd>
2009-03-31 14:17:55 +0000

    Added option to wrap the runnable with an Executor, specified by a sytem property

--- trunk/trinidad/transactionalrunner/src/main/java/com/neuri/trinidad/transactionalrunner/RollbackBean.java
+++ trunk/trinidad/transactionalrunner/src/main/java/com/neuri/trinidad/transactionalrunner/RollbackBean.java
@@ -1,11 +1,19 @@
 package com.neuri.trinidad.transactionalrunner;
+
+import java.util.concurrent.Executor;
+
 import org.springframework.transaction.annotation.Transactional;
 
-	 
 public class RollbackBean implements RollbackIntf {
-	@Transactional(rollbackFor=Throwable.class)
-	public void process(Runnable r) {
-		r.run();
-		throw new RollbackNow();
-	}
+    @Transactional(rollbackFor = Throwable.class)
+    public void process(Runnable r) {
+        if (System.getProperty("trinidad.test.wrapper") != null) {
+            Executor testWrapper = (Executor) FitnesseSpringContext.getInstance().getBean(
+                    System.getProperty("trinidad.test,wrapper"));
+            testWrapper.execute(r);
+        } else {
+            r.run();
+        }
+        throw new RollbackNow();
+    }
 }
