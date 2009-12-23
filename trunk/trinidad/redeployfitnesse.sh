db959a81f6669dacb71f6fc8658dc7dd23acca12
gojkoa <gojkoa@d5e6a7ba-fa0c-11dd-99c0-75d572ba1ddd>
2009-12-23 23:11:13 +0000

    support for -c fitnesse command

--- trunk/trinidad/redeployfitnesse.sh
+++ trunk/trinidad/redeployfitnesse.sh
@@ -1,1 +1,2 @@
-mvn deploy:deploy-file -Durl=sftp://neuri.com/home/webadmin/maven.neuri.com/html -Dfile=/tmp/fitnesse.jar -DgroupId=org.fitnesse -DartifactId=fitnesse -Dversion=20091121 -Dpackaging=jar -DrepositoryId=neuri-maven
+mvn deploy:deploy-file -Durl=sftp://neuri.com/home/webadmin/maven.neuri.com/html -Dfile=/tmp/fitnesse.jar -DgroupId=org.fitnesse -DartifactId=fitnesse -Dversion=20091221-SNAPSHOT -Dpackaging=jar -DrepositoryId=neuri-maven
+#mvn install:install-file -Dfile=/tmp/fitnesse.jar -DgroupId=org.fitnesse -DartifactId=fitnesse -Dversion=20091221-SNAPSHOT -Dpackaging=jar
